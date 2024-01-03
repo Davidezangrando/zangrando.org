8dac0697744c22bc7568cfbe2997f41362573e97
Davidezangrando <75436804+Davidezangrando@users.noreply.github.com>
2024-01-03 16:18:01 +0100

    modifica config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite' ;
 
 export default defineConfig ({
+  
   server: {
+    https: {
+      key: './privkey.key',
+      cert: './cert.key',
+    },
     host: true, 
     port: 80,
   },
